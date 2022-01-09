b04f099ce9571c8dfe9f5e0db687d3b423ea10e0
bitofbeans <benas4sun@gmail.com>
2022-01-09 14:27:05 -0600

    Updated world rendering and documentation

--- main.py
+++ main.py
@@ -16,17 +16,16 @@ screen = pygame.display.set_mode((screenWidth,screenHeight))
 pygame.display.set_caption('Platformer')
 
 # game variables
-tile_size = 200
-
+tile_size = 50
 
 # image imports
 sun_img = pygame.image.load('img/sun.png')
 sun_img = pygame.transform.scale(sun_img, (75, 75))
 bg_img = pygame.image.load('img/sky.png')
 
-# draw grid
+# draw grid for entire map
 def drawGrid():
-  for line in range(0,6):
+  for line in range(0,int((screenWidth/tile_size)+1)):
       lineLen = line * tile_size
       pygame.draw.line(screen,WHITE,(0, lineLen), (screenWidth, lineLen))
       pygame.draw.line(screen,WHITE,(lineLen, 0), (lineLen, screenHeight))
@@ -35,12 +34,15 @@ def drawGrid():
 # define world
 class World():
     def __init__(self,data):
+      # clear list
       self.tile_list = []
       
       # load images
       dirt_img = pygame.image.load('img/dirt.png')
       grass_img = pygame.image.load('img/grass.png')
+      err = pygame.image.load('img/unknown.png')
       
+      # extract tiles from data
       row_count = 0
       for row in data:
           col_count = 0
@@ -48,8 +50,10 @@ class World():
               if tile != 0:
                   if tile == 1:
                       img = pygame.transform.scale(dirt_img,(tile_size,tile_size))
-                  if tile == 2:
+                  elif tile == 2:
                       img = pygame.transform.scale(grass_img,(tile_size,tile_size))
+                  else: img = pygame.transform.scale(err,(tile_size,tile_size))
+
                   img_rect = img.get_rect()
                   img_rect.x = col_count * tile_size
                   img_rect.y = row_count * tile_size
@@ -60,17 +64,33 @@ class World():
         
     def draw(self):
         for tile in self.tile_list:
+           # draw each tile
            screen.blit(tile[0],tile[1])
 
-
+# set world data
 world_data =[
-[1, 1, 1, 1, 1],
-[1, 0, 0, 0, 1],
-[1, 0, 0, 0, 1],
-[1, 0, 0, 0, 1],
-[1, 2, 2, 2, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
-
+# create world sprite from data
 world = World(world_data)
 
 # --- game loop
